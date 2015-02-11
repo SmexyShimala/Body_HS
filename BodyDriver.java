25272d4eb31fcb1a9f302416919c206c39a133e9
SmexyShimala <jshimala@saintjoe.edu>
2015-02-11 14:04:50 -0600

    Update BodyDriver.java

--- BodyDriver.java
+++ BodyDriver.java
@@ -1,13 +1,19 @@
+/*
+	BodyDriver: a class to demonstrate and manipulate Body objects
+	code from book typed in by Houston Shimala
+	11 February 2015
+								*/
 public class BodyDriver {
-	public static void main(String[] args) {
-		Body sun = new Body();
-		sun.idNum = Body.nextID++;
-		sun.name = "Sol";
-		sun.orbits = null; // Sun doesnt orbit
-		
-		Body earth = new Body();
-		earth.idNum = Body.nextID++;
-		earth.name = "Earth";
-		earth.orbits = sun;
+  public static void main(String[] args) {
+	Body sun = new Body();
+	// sun.idNum = Body.nextID++;
+	sun.name = "Sol";
+	sun.orbits = null; // Sun don't orbit nobody
+	System.out.println(sun.toString());
+
+	Body earth = new Body("Earth", sun);
+	// earth.idNum = Body.nextID++;
+	earth.orbits = sun;
+	System.out.println(earth.toString());
 	}
 }
