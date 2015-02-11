d61e1be47298f9e9c7fcf247ef5e642212413947
SmexyShimala <jshimala@saintjoe.edu>
2015-02-11 14:05:21 -0600

    Update Body.java

--- Body.java
+++ Body.java
@@ -2,10 +2,32 @@
 // Code from page 42 
 // Class name Body
 
+// Additional code from book typed in by BLC 11 Feb 2015
+
 class Body {
 	public long idNum;
 	public String name;
 	public Body orbits;
 	
 	public static long nextID = 0;
+
+	// Constructors (two of them here; there could be more)
+	Body() {
+	   idNum = nextID++;
+	   }
+	
+	Body(String bodyName, Body orbitsAround) {
+	   this();
+	   name = bodyName;
+	   orbits = orbitsAround;
+	   }
+
+	// Most classes will need a toString() so we can visualize the object
+	public String toString() {
+	   String desc = idNum + " (" + name + ")";
+	   if (orbits != null) 
+		desc += " orbits " + orbits.toString();
+	   return desc;
+	   }
+
 }
